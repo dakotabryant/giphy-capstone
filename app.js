58e76bd7e802f8863e46c2bf5b5d244bf7da5bcf
Scylol <djd3dobbin@gmail.com>
2017-04-13 16:04:34 -0400

    started adding functionality

--- app.js
+++ app.js
@@ -1,47 +1,70 @@
-const apiKey = 'AIzaSyDB2rv_p72WbrIQErMhVCM61-QZb0TFEJ0';
-const endpointURL = "https://www.googleapis.com/youtube/v3/search";
+let gifArray = [];
+let round1 = [[],[],[],[]];
+let round2 = [[],[]];
+let round3 = [];
+
 
 $(function(){
+  const apiKey = "dc6zaTOxFJmzC";
+  const endpointURL = "http://api.giphy.com/v1/gifs/search";
 
-  function getData(searchTerm, callback){
+  
+function getData(searchTerm, callback){
     const query={
-      part:'snippet',
-      key:apiKey,
-      q:searchTerm,
-      maxResults: 24
+      api_key:apiKey,
+      q: searchTerm,
+      limit: 100
     }
     $.getJSON(endpointURL, query, callback);
   }
 
 
-  function showThumbnail(data){
-    let thumbnails = `<p>No Results</p>`;
-    console.log(data);
-    if (data.items) {
-      thumbnails = data.items.map(function(item){
-        if(item.id.kind === "youtube#channel") {
-          return `<div class="video-container"><a target="_blank" href="https://www.youtube.com/channel/${item.id.channelId}"><img src=" ${item.snippet.thumbnails.high.url} " /></a><p class='video-title'>${item.snippet.title}</p></div>`
-        } else {
-          return `<div class="video-container"><a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}"><img src=" ${item.snippet.thumbnails.high.url} " /></a><p class='video-title'>${item.snippet.title}</p></div>`
-        }
 
-      })
-    }
+//modification functions
 
-    $('.js-container').append(thumbnails);
-  }
-  $('.search-form').submit(function(event) {
-    event.preventDefault();
-    if ($('.search-form').hasClass('at-top')) {
-      $('.search-form, .logo').toggleClass('')
-    } else {
-      $('.search-form, .logo').toggleClass('at-top')
-    }
-
-    $('.js-container').empty();
-    let searchTerm = $('#search-box').val();
-      getData(searchTerm, showThumbnail);
+function getGifs(object) {
+  object.data.map(function(item) {
+  gifArray.push(item.url);
   })
+  console.log(gifArray);
+displayRound(gifArray);
+displayRound(gifArray);
+}
+function displayRound(arr) {
+  console.log(arr[Math.floor(Math.random()*arr.length)]);
+  // console.log(str)
+}
+getData('the office', getGifs);
+})
+
+  // function showThumbnail(data){
+  //   let thumbnails = `<p>No Results</p>`;
+  //   console.log(data);
+  //   if (data.items) {
+  //     thumbnails = data.items.map(function(item){
+  //       if(item.id.kind === "youtube#channel") {
+  //         return `<div class="video-container"><a target="_blank" href="https://www.youtube.com/channel/${item.id.channelId}"><img src=" ${item.snippet.thumbnails.high.url} " /></a><p class='video-title'>${item.snippet.title}</p></div>`
+  //       } else {
+  //         return `<div class="video-container"><a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}"><img src=" ${item.snippet.thumbnails.high.url} " /></a><p class='video-title'>${item.snippet.title}</p></div>`
+  //       }
+
+  //     })
+  //   }
+
+  //   $('.js-container').append(thumbnails);
+  // }
+  // $('.search-form').submit(function(event) {
+  //   event.preventDefault();
+  //   if ($('.search-form').hasClass('at-top')) {
+  //     $('.search-form, .logo').toggleClass('')
+  //   } else {
+  //     $('.search-form, .logo').toggleClass('at-top')
+  //   }
+
+  //   $('.js-container').empty();
+  //   let searchTerm = $('#search-box').val();
+  //     getData(searchTerm, showThumbnail);
+  // })
 
 //item.snippet.thumbnails.high.url
 
@@ -58,4 +81,4 @@ $(function(){
 //}
 
 
-})
+
