87fd2eedcd9cc96edf4641039b80f24bc379b977
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-13 18:43:56 -0500

    Refactored

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
-$(function(){
-let gifArray = [];
-const apiKey = "dc6zaTOxFJmzC";
-const endpointURL = "http://api.giphy.com/v1/gifs/search";
-let user_input;
+$(function() {
+    let gifArray = [];
+    let currentIndex = 1;
+    let championIndex = 0;
+    const apiKey = "dc6zaTOxFJmzC";
+    const endpointURL = "http://api.giphy.com/v1/gifs/search";
+    let user_input;
 
-function getData(searchTerm, callback){
-    const query={
-      api_key:apiKey,
-      q: searchTerm,
-      limit: 100
+    function getData(searchTerm, callback) {
+        const query = {
+            api_key: apiKey,
+            q: searchTerm,
+            limit: 100
+        }
+        $.getJSON(endpointURL, query, callback);
     }
-    $.getJSON(endpointURL, query, callback);
-  }
 
-
-function getGifs(object) {
-  object.data.map(function(item) {
-  gifArray.push(item.images.downsized_large.url);
-  })
-  console.log(gifArray);
-}
-
-$('.search-form').submit(function(e){
-  user_input = $('#search-box').val();
-  getData(user_input, getGifs);
-  e.preventDefault();
-  setTimeout(function(){
-    $('.landing-page, .img-round').toggleClass('hidden');
-   document.getElementById('#img1').src = `${gifArray[0]}`;
-   document.getElementById('#img2').src = `${gifArray[1]}`;
-  
- }, 1000)
-  
-  
-})
-//if click was index 0 , remove index 1, if click was index1 remove index zero
-$('.img-block').on('click', 'img', function(event){
-  console.log(event);
-
-    if($(this).attr("src")===gifArray[0]) {
-      gifArray.splice(1,1);
+    function buildGifArray(obj) {
+        obj.data.map(function(item) {
+            gifArray.push(item.images.downsized_large.url);
+        })
     }
-      else
-      {
-        gifArray.splice(0,1);
-      }
-         document.getElementById('#img1').src = `${gifArray[0]}`;
-         document.getElementById('#img2').src = `${gifArray[1]}`;
-   
-    
-  })
+    function renderImages() {
+      $('.landing-page, .img-round').toggleClass('hidden');
+      document.getElementById('#img1').src = `${gifArray[0]}`;
+      document.getElementById('#img2').src = `${gifArray[1]}`;
+    }
+    function renderView(data) {
+      buildGifArray(data);
+      renderImages();
+    }
+
+    $('.search-form').submit(function(e) {
+        e.preventDefault();
+        user_input = $('#search-box').val();
+        getData(user_input, renderView);
+    })
+
+    //if click was index 0 , remove index 1, if click was index1 remove index zero
+    $('.img-block').on('click', 'img', function(event) {
+        console.log(event);
+        if ($(this).attr("src") !== gifArray[championIndex]) {
+          championIndex = currentIndex;
+        }
+        currentIndex++;
+        document.getElementById('#img1').src = `${gifArray[championIndex]}`;
+        document.getElementById('#img2').src = `${gifArray[currentIndex]}`;
+    })
 })
 
 
@@ -59,4 +55,3 @@ $('.img-block').on('click', 'img', function(event){
 // set up current champion graphic
 //fix image block height
 // Check if set timeout will fuck us up // alternative
-  
