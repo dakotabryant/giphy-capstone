cb0a4a4e2dbb6dac237325610a2c6637161e7308
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-14 16:05:13 -0500

    Mobile styling, comments

--- scripts/app.js
+++ scripts/app.js
@@ -10,51 +10,51 @@ $(function() {
     const endpointURL = 'http://api.giphy.com/v1/gifs/search';
     let user_input;
 
+//JSON Request to Giphy
     function getData(query, callback) {
         query.api_key = apiKey;
         query.limit = 100;
 
         $.getJSON(endpointURL, query, callback);
     }
-
+//Grabs the data from Giphy, and puts it in to an array
     function buildGifArray(obj) {
         obj.data.map(function(item) {
             gifArray.push(item.images.downsized_large.url);
         })
     }
-
+// Hides the landing page, and adds first two images to the view
     function renderImages() {
         $('.landing-page').addClass('hidden');
         $('.img-round').removeClass('hidden');
         updateSrcs();
         currentOffset+=100;
     }
-
+// Function to update the srcs when we have a new set of images
     function updateSrcs() {
         $('#img1').attr('src', `${gifArray[championIndex]}`);
         $('#img2').attr('src', `${gifArray[challengerIndex]}`);
     }
+// Function to update the urls attached to the buttons
     function updateUrls(){
       championUrl = gifArray[championIndex];
       challengerUrl = gifArray[challengerIndex];
       $('#championButton').attr('href', `${championUrl}`);
       $('#challengerButton').attr('href', `${challengerUrl}`)
     }
-
+// Renders our view, which shows images, and updates the Urls accordingly
     function renderView(data) {
         buildGifArray(data);
         renderImages();
         updateUrls();
     }
-    
 
+// On-click functions
     $('.search-form').submit(function(e) {
         e.preventDefault();
         user_input = $('#search-box').val();
         getData({q: user_input}, renderView);
     })
-
-    //if click was index 0 , remove index 1, if click was index1 remove index zero
     $('.img-block').on('click', 'img', function(event) {
         if ($(this).attr('src') !== gifArray[championIndex]) {
             championIndex = challengerIndex;
@@ -68,6 +68,3 @@ $(function() {
             updateSrcs();
     })
 })
-
-
-
