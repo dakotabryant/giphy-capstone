d1fca8938bd20f28c3fe6d6574fbe740c7423a72
htwright <harrisontwright@gmail.com>
2017-04-16 08:31:20 -0500

    review

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+    //--Harrison-- It seems that if a state object was used, we could combine updateSrcs and renderImages, since we could manage
+    //which page we were on with a global variable instead of via classes. Though this would definetly require a lot of changes
+    //elsewhere in the program, and would likely not be worth the time investment. The program is very clean and readable,
+    //the styling is consistant, the function names make sense, the logic of the program is nicely apportioned into neat, appropriately
+    //named functions such that no singular function is hard to understand. For what it does, this program is very lean, at least in the
+    //JS department.
+
+
 $(function() {
     let gifArray = [];
     let championIndex = 0;
@@ -30,6 +38,7 @@ $(function() {
         updateSrcs();
         currentOffset+=100;
     }
+
 // Function to update the srcs when we have a new set of images
     function updateSrcs() {
         $('#img1').attr('src', gifArray[championIndex]);
